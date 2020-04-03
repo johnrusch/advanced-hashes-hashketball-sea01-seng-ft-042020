f6675950e57f3b538a9affb35baa7a246a4e0ca7
John Rusch <johnrusch7@gmail.com>
2020-04-03 23:02:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -192,6 +192,7 @@ def big_shoe_rebounds
   biggest_shoes = []
   big_shoes.push(game_hash[:home][:players].max_by { |n| n[:shoe] })
   big_shoes.push(game_hash[:away][:players].max_by { |n| n[:shoe] })
+  binding.pry
   biggest_shoes.push(big_shoes.max_by { |n| n[:shoe] })
   biggest_shoes[:rebounds]
   binding.pry
